af503f2af3229752648dc3f01b634541a0967c13
zakhy <34194927+zakhy@users.noreply.github.com>
2017-12-07 14:56:20 +0100

    Add files via upload

---
+++ miejsca_zerowe.py
@@ -0,0 +1,22 @@
+import math
+a = int(input("Podaj wartość a: "))
+b = int(input("Podaj wartość b: "))
+c = int(input("Podaj wartość c: "))
+zerowka = int(2-2)
+delta = int((b*b)-(4*a*c))
+pdelta = math.sqrt(delta)
+bcd = int(b*(-1))
+print('Delta wynosi: ' + str(delta) + ' a pierwiastek z niej wynosi ' + str(pdelta) + ".")
+if delta==zerowka:
+ wyjs0 = (bcd/(2*a))
+ print("Oto miejsce zerowe:" + str(wyjs0))
+if delta<0:
+ print("Delta jest ujemna więc nie będzie żadnych miejsc zerowych :(")
+if delta>0:
+ wyjs1 = ((bcd+pdelta)/(2*a))
+ wyjs2 = ((bcd-pdelta)/(2*a))
+ print("Oto miejsca zerowe:" + str(wyjs1) + " i " + str(wyjs2) + ".")
+ 
+
+
+
